b7bbbbbaa5f08eb872f0a0534bcea82c5e0e7f06
sflorezo <saf9215@stern.nyu.edu>
2025-11-13 18:36:41 -0500

    Add tomli handler

--- src/pysfo/basic/__init__.py
+++ src/pysfo/basic/__init__.py
@@ -2,7 +2,8 @@ from .date_utils.convert_dates import *
 from .geo_utils.country_groups import *
 from .basicfns import *
 from .pandas_utils.groupby_apply_various import *
+from .pandas_utils.other_various import *
 from .regfns import *
 from .time_utils import *
 from .io_tools import *
-from .pandas_utils.other_various import *
+
---
+++ src/pysfo/basic/io_tools/__init__.py
@@ -0,0 +1,2 @@
+from .config_loader import *
+from .io_tools_various import *
