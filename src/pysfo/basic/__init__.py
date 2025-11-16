9e9e250cebb3a9d8ef6e8e8247f7659de9279547
sflorezo <saf9215@stern.nyu.edu>
2025-11-15 22:51:20 -0500

    Add lazy-import

--- src/pysfo/basic/__init__.py
+++ src/pysfo/basic/__init__.py
@@ -1,6 +1,7 @@
 from .date_utils.convert_dates import *
 from .geo_utils.country_groups import *
 from .basicfns import *
+from .lazy_import import *
 from .pandas_utils.groupby_apply_various import *
 from .pandas_utils.other_various import *
 from .regfns import *
